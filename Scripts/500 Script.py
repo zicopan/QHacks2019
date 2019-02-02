9df158b6aeb68f1ff62d911b7c2948de70a858c7
Andrew Deschenes <deschena@mcmaster.ca>
2019-02-02 15:31:46 -0500

    fixing bug in script

--- Scripts/500 Script.py
+++ Scripts/500 Script.py
@@ -6,12 +6,13 @@ import itertools
 #Output directory
 out_dir = "MLpics/"
 
-#list of folders
+#list of folders 
 feature_list = ["Eyeglasses", "Wearing_Hat", "Wearing_Lipstick", "No_Beard"]
 
 os.makedirs(out_dir)
 
 for folder in feature_list:
     os.makedirs(out_dir + folder)
+    files = os.listdir(folder)
     for i in range(500):
-        shutil.copy(folder, out_dir + folder)
+        shutil.copy(folder + "/" + files[i], out_dir + folder)
