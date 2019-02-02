55e189c6d1b687372f47bf9e022bb91e629c6672
Nico Stepan <32588320+zicopan@users.noreply.github.com>
2019-02-02 16:07:40 -0500

    added code files

---
+++ App.js
@@ -0,0 +1,38 @@
+// CarMan 
+/*
+Nico, Maanav, Kshitij
+*/
+
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
+import {
+  StackNavigator,
+  createStackNavigator,
+} from 'react-navigation';
+
+import OpenScreen from './screens/OpenScreen.js'
+import HomeScreen from './screens/HomeScreen.js'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Navi />
+    );
+  }
+}
+
+const Navi = createStackNavigator({
+  Open: OpenScreen,
+  Home: HomeScreen
+},
+{
+  headerMode: 'none',
+  navigationOptions: {
+    headerVisible: false,
+  }
+ }
+);
+
+
+
+
