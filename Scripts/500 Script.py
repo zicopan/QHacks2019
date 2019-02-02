3144ad851913da12f271b0e13dd0d79b9b964d1c
auchimaru <aidan.uchimaru@gmail.com>
2019-02-02 14:19:42 -0500

    Add files via upload

---
+++ Scripts/500 Script.py
@@ -0,0 +1,20 @@
+import glob
+import shutil
+import os
+import itertools
+
+#Output directory
+out_dir = "MLpics/"
+
+#list of folders 
+feature_list = [Eyeglasses, Wearing_Hat, Wearing_Lipstick, No_Beard]
+
+
+os.makedirs(out_dir)
+
+for folder in feature_list:
+    os.makedirs(out_dir + str(folder))
+    src_dir = str(folder) + "/"
+    for i in range(500):
+        shutil.copy(src_dir, out_dir + str(folder))
+        
